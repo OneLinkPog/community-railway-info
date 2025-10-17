64cc0027ae406425950e599d883267ce53c3a24c
Juliandev02 <julian@strawberryfoundations.org>
2025-10-17 21:26:27 +0200

    chore: clean up whitespace and improve formatting in index route

--- core/routes/index.py
+++ core/routes/index.py
@@ -7,25 +7,26 @@ import re
 
 index = Blueprint('index', __name__)
 
+
 @index.route('/')
 def index_route():
     user = session.get('user')
-    
+
     with open(main_dir + '/lines.json') as f:
         lines = json.load(f)
-    
+
     with open(main_dir + '/operators.json') as f:
         operators = json.load(f)
-    
+
     operator = None
     admin = False
-    
+
     if user and 'id' in user:
         operator = [op for op in operators if user['id'] in op['users']]
-        
+
     if user and user["id"] in config.web_admins:
         admin = True
-    
+
     line_types = {
         'public': {'suspended': [], 'partially_suspended': [], 'running': [], 'possible_delays': [], 'no_scheduled': []},
         'private': {'suspended': [], 'partially_suspended': [], 'running': [], 'possible_delays': [], 'no_scheduled': []},
@@ -33,7 +34,7 @@ def index_route():
         'tram': {'suspended': [], 'partially_suspended': [], 'running': [], 'possible_delays': [], 'no_scheduled': []},
         "bus": {'suspended': [], 'partially_suspended': [], 'running': [], 'possible_delays': [], 'no_scheduled': []},
     }
-    
+
     for line in lines:
         if 'notice' in line:
             if line['notice'] is None or line['notice'].strip() == '':
@@ -42,7 +43,7 @@ def index_route():
                 line['notice'] = line['notice'].strip()
         else:
             line['notice'] = None
-                
+
         line_type = line.get('type', 'public')
         status_key = {
             'Suspended': 'suspended',
@@ -55,15 +56,16 @@ def index_route():
         if status_key and line_type in line_types:
             line_types[line_type][status_key].append(line)
 
-    
     # Sort each status list in each line type
     for line_type in line_types.values():
         for status in line_type.values():
             status.sort(key=lambda x: (
-                ''.join([i for i in x['name'] if not i.isdigit()]),  # alphabetical part
-                [int(''.join(g)) for g in re.findall(r'\d+', x['name'])] or [0]  # numerical part
+                # alphabetical part
+                ''.join([i for i in x['name'] if not i.isdigit()]),
+                [int(''.join(g)) for g in re.findall(
+                    r'\d+', x['name'])] or [0]  # numerical part
             ))
-    
+
     return render_template(
         'index.html',
         user=user,
@@ -74,17 +76,18 @@ def index_route():
         maintenance_message=config.maintenance_message
     )
 
+
 @index.route('/computercraft-setup')
 def computercraft_setup_route():
     user = session.get('user')
-    
+
     with open(main_dir + '/operators.json') as f:
         operators = json.load(f)
-    
+
     operator = None
     if user and 'id' in user:
         operator = [op for op in operators if user['id'] in op['users']]
-    
+
     return render_template(
         'computercraft-setup.html',
         user=user,
